bf41f356624861202ab87a0afaec6e060378cab7
3443718488@qq.com <3443718488@qq.com>
2022-07-04 17:13:27 +0800

    fix

--- src/store/modules/app/index.ts
+++ src/store/modules/app/index.ts
@@ -1,22 +1,19 @@
 import { defineStore } from 'pinia'
 
-interface BaseAppInfo {
-    isCollapse: boolean
-}
-
 export const useAppStore = defineStore({
-    id: 'app',
-    state: () => ({
-        isCollapse: {} as BaseAppInfo,
-    }),
-    getters: {
-        getCollapse: (state) => { return state.isCollapse }
-    },
-    actions: {
-        handleCollapse(status) {
-            console.log("status", status);
-            status == false ? this.isCollapse = false : this.isCollapse = true
-            return this.isCollapse
-        }
+  id: 'app',
+  state: () => ({
+    isCollapse: false,
+  }),
+  getters: {
+    getCollapse: (state) => {
+      return state.isCollapse
     }
+  },
+  actions: {
+    handleCollapse(status) {
+      status == false ? this.isCollapse = false : this.isCollapse = true
+      return this.isCollapse
+    }
+  }
 })
