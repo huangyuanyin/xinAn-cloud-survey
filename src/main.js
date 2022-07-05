0adabfbd3274c3060168cb1140405d7ef5102638
liuzhanghao <liuzhanghao@infosec.com.cn>
2022-07-05 10:27:46 +0800

    引入echarts5

--- src/main.js
+++ src/main.js
@@ -5,13 +5,16 @@ import { createPinia } from "pinia"
 
 import 'element-plus/theme-chalk/el-message.css';
 
+import echarts from './utils/echarts';
+
 // createApp(App).mount('#app')
 const pinia = createPinia()
 const app = createApp(App)
 
+app.config.globalProperties.$echarts = echarts
+
 app.use(router)
 app.use(pinia)
-
 import './permission'
 
 app.mount('#app')
