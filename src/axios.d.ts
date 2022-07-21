7deeab0d3fe94157549580bc8f8334d1238b70c1
liuzhanghao <liuzhanghao@infosec.com.cn>
2022-07-21 10:56:50 +0800

    axios.d.ts

---
+++ src/axios.d.ts
@@ -0,0 +1,8 @@
+import * as axios from 'axios'
+ 
+declare module 'axios' {
+   interface AxiosInstance {
+      // eslint-disable-next-line no-undef
+      (config: AxiosRequestConfig): Promise<any>
+   }
+}
