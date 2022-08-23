5e5677bda2aa34219b455ba64f7b6c5b3d321203
liuzhanghao <3443718488@qq.com>
2022-08-23 17:19:24 +0800

    换websocket地址

--- src/components/Termail.vue
+++ src/components/Termail.vue
@@ -52,7 +52,7 @@ export default {
     })
     this.term.open(terminalContainer)
     // open websocket
-    this.terminalSocket = new WebSocket('ws://10.20.86.27:0303/terminal/wensock?host_ip=10.20.86.27&host_por=22&user=root&passwd=inf0sec312!')
+    this.terminalSocket = new WebSocket('ws://10.20.86.27:1182/terminal/wensock?host_ip=10.20.86.27&host_por=22&user=root&passwd=inf0sec312!')
     this.terminalSocket.onopen = this.runRealTerminal
     this.terminalSocket.onclose = this.closeRealTerminal
     this.terminalSocket.onerror = this.errorRealTerminal
